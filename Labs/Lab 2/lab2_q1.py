b2eadb547cf8e66a4fe08b01f5e0fd5be8049475
SeyedPooya Soofbaf <spsoofbaf@gmail.com>
2023-02-09 13:34:58 +0330

    add skimage

---
+++ Labs/Lab 2/lab2_q1.py
@@ -0,0 +1,10 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage import data
+from skimage import transform
+
+if __name__ == '__main__':
+    print("test")
+    pass
